05255a17dd7d192c40904a860c4e2e541a032f31
Jeff <jeff.vera@gmail.com>
2012-03-06 15:20:30 -0500

    Initial commit.

---
+++ RangeKata/Range.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeKata
+{
+	public class Range
+	{
+		public int Low { get; set; }
+		public int High { get; set; }
+
+		public Range(
+			int lowEndOfRange, 
+			int highEndOfRange, 
+			LowRangeEndInclusivity lowEndInclusivity, 
+			HighRangeEndInclusivity highEndInclusivity)
+		{
+			Low = lowEndOfRange;
+			High = highEndOfRange;
+			LowEndInclusivity = lowEndInclusivity;
+			HighEndInclusivity = highEndInclusivity;			
+		}
+
+		public LowRangeEndInclusivity LowEndInclusivity { get; set; }
+		public HighRangeEndInclusivity HighEndInclusivity { get; set; }
+
+	}
+}
---
+++ RangeKata/RangeTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace RangeKata
+{
+	[TestFixture]
+	public class RangeTests
+	{
+		[Test]
+		public void CanInstantiateTest()
+		{
+			Range r = new Range(
+				2, 
+				6, 
+				LowRangeEndInclusivity.IsInclusive,
+				HighRangeEndInclusivity.IsExclusive);
+
+			Assert.That(r, Is.Not.Null);
+			Assert.That(r.Low, Is.EqualTo(2));
+			Assert.That(r.High, Is.EqualTo(6));
+			Assert.That(
+				r.LowEndInclusivity, 
+				Is.EqualTo(LowRangeEndInclusivity.IsInclusive));
+			Assert.That(
+				r.HighEndInclusivity,
+				Is.EqualTo(HighRangeEndInclusivity.IsExclusive));
+		}
+	}
+}
